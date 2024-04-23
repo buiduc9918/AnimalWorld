44a4c8c1268db0084addd85c05972f5e02104772
buiduc9918 <97076379+buiduc9918@users.noreply.github.com>
2024-04-23 11:10:28 +0700

    Update 3

---
+++ Assets/Play.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+}
